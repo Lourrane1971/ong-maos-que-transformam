224019da7850ff656d3ede840adf3c836d77452c
Lourrane Almeida <lourranemarchini@gmail.com>
2026-09-25 16:51:48 -0300

    fix: corrige build e navegacao em producao

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: 'html',
+
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+
+    rollupOptions: {
+      input: {
+        index: resolve(process.cwd(), 'html/index.html'),
+        projetos: resolve(process.cwd(), 'html/projetos.html'),
+        cadastro: resolve(process.cwd(), 'html/cadastro.html')
+      }
+    }
   }
 })
